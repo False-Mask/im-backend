07a8702478698be9d4c867beece3bf7edcf59547
Zhiqiang <2623036785@qq.com>
2022-12-27 22:26:47 +0800

    :memo: 项目初始化

---
+++ src/main/kotlin/com/example/im/ImApplication.kt
@@ -0,0 +1,11 @@
+package com.example.im
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImApplication
+
+fun main(args: Array<String>) {
+    runApplication<ImApplication>(*args)
+}
